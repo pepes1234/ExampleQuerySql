6e06bb48dc7095327129eee34caa3e3f08d64fa6
= <gabrielvalengasilva412@gmail.com>
2022-10-24 15:08:57 -0300

    Exemplos

---
+++ Model/QueryExampleContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace sqlQueryExample.Model
+{
+    public partial class QueryExampleContext : DbContext
+    {
+        public QueryExampleContext()
+        {
+        }
+
+        public QueryExampleContext(DbContextOptions<QueryExampleContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=SNCCHLAB02F03\\TEW_SQLEXPRESS;Initial Catalog=QueryExample;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.ToTable("Usuario");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Nome)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("senha");
+
+                entity.Property(e => e.Sobrenome)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using sqlQueryExample.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+List<string> Nomes = new List<string>();
+List<string> Sobrenomes = new List<string>();
+List<string> Objetos = new List<string>();
+List<string> Adjetivos = new List<string>();
+List<string> Caracteres = new List<string>();   
+
+Nomes.Add("Pedro");
+Nomes.Add("João");
+Nomes.Add("DAVI.");
+Nomes.Add("Jhonny");
+Nomes.Add("Gabriel");
+Nomes.Add("Clara");
+Nomes.Add("Elisa");
+Nomes.Add("Mateus");
+Nomes.Add("Guilherme");
+Nomes.Add("Vinicius");
+Nomes.Add("Alice");
+
+Sobrenomes.Add("Silva");
+Sobrenomes.Add("Souza");
+Sobrenomes.Add("Pepes");
+Sobrenomes.Add("Ferraz");
+Sobrenomes.Add("Piratelo");
+Sobrenomes.Add("Padrin");
+Sobrenomes.Add("Pain");
+Sobrenomes.Add("Copas");
+Sobrenomes.Add("Ouros");
+Sobrenomes.Add("Bispo");
+Sobrenomes.Add("Cabello");
+
+Objetos.Add("Carro");
+Objetos.Add("Avião");
+Objetos.Add("Quadro");
+Objetos.Add("Televisão");
+Objetos.Add("Celular");
+Objetos.Add("Fone");
+Objetos.Add("Teclado");
+Objetos.Add("Piano");
+Objetos.Add("Guitarra");
+Objetos.Add("Caixa");
+Objetos.Add("Monitor");
+
+Adjetivos.Add("Atraentes");
+Adjetivos.Add("Doces");
+Adjetivos.Add("Amargos");
+Adjetivos.Add("Diferentes");
+Adjetivos.Add("Sapecas");
+Adjetivos.Add("Curiosos");
+Adjetivos.Add("Estranhos");
+Adjetivos.Add("Feios");
+Adjetivos.Add("Lindos");
+Adjetivos.Add("Legals");
+Adjetivos.Add("Chatos");
+
+Caracteres.Add("/");
+Caracteres.Add(".");
+Caracteres.Add(",");
+Caracteres.Add("1");
+Caracteres.Add("2");
+Caracteres.Add(";");
+Caracteres.Add("");
+Caracteres.Add("'");
+Caracteres.Add("{");
+Caracteres.Add("y");
+Caracteres.Add(".");
+
+
+//context = variavel de acesso ao banco de dados
+QueryExampleContext context = new QueryExampleContext();
+
+
+// void CriarUsuarioAleatorio()
+// {
+//     string senhausuario = "";
+//     Random rnd = new Random();
+//     Usuario usr = new Usuario();
+//     usr.Nome = Nomes[rnd.Next(0, Nomes.Count)] + " " + Sobrenomes[rnd.Next(0, Sobrenomes.Count)];
+//     for(var i = 0; i<rnd.Next(10, 16); i++)
+//     {
+//         senhausuario += Caracteres[rnd.Next(0, Caracteres.Count)];
+//     }
+//     usr.Senha = senhausuario;
+//     context.Add(usr);
+//     context.SaveChanges();
+// }
+
+//função = {nivel de proteção} {nome função} {parametros} exemplo: public string Mensagem(string Mensagem);
+
+// void InserirUsuario(Usuario usr)
+// {
+//     context.Add(usr);
+//     context.SaveChanges();
+// }
+
+// Usuario usr1 = new Usuario();
+// usr1.Nome = "Paulo";
+// usr1.Sobrenome = "Silva";
+// usr1.Senha = "1234";
+
+// InserirUsuario(usr1);
+
+//requisicao dos usuarios de 21 a 30
+var query1 = from usr in context.Usuarios.Skip(20).Take(10) select usr;
+
+foreach(var users in query1)
+{
+    Console.WriteLine($"{users.Id} | {users.Nome} | {users.Sobrenome} | {users.Senha}");
+}
+
+//requisicao dos 10 primeiros usuarios da lista SEM ID
+var query2 = from usr in context.Usuarios.Take(10) select usr;
+
+foreach(var users in query2)
+{
+    Console.WriteLine($"{users.Id} | {users.Nome} | {users.Sobrenome} | {users.Senha}");
+}
+
+//requisição dos primerios 10 usuarios pelo id
+var query3 = from usr in context.Usuarios where usr.Id <= 10 select usr;
+
+foreach(var users in query3)
+{
+    Console.WriteLine($"{users.Id} | {users.Nome} | {users.Sobrenome} | {users.Senha}");
+}
